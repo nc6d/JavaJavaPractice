c684528fd49b1cc74281e020863c478763d09a53
nc6d <6od92002@gmail.com>
2021-01-15 12:19:20 +0200

    15.01

---
+++ src/new_task/Main.java
@@ -0,0 +1,39 @@
+package new_task;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+
+        System.out.println("Rows: ");
+        int rows = sc.nextInt();
+
+        int[] array = new int[rows];
+
+        for (int i = 0; i < rows; i++) {
+            array[i] = rnd.nextInt(10);
+        }
+
+        int maxSum = 0;
+        int maxSumIndex = 0;
+        for (int i = 0; i < rows - 2; i++) {
+            int sum = 0;
+            for (int j = i; j < i + 3; j++) {
+                sum += array[j];
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxSumIndex = i;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(maxSum);
+        System.out.println(maxSumIndex);
+
+    }
+}
